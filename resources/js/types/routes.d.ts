ed04d422084d3fc2f03458b31e4cebabf8e5521b
nckrtl <nick.retel@gmail.com>
2025-03-27 13:09:54 +0100

    .gitignore reindexed

--- resources/js/types/routes.d.ts
+++
@@ -1,5 +0,0 @@
-import "momentum-trail"
-
-declare module "momentum-trail" {
-    export interface RouterGlobal {"url":"http:\/\/srpm.test","port":null,"defaults":[],"routes":{"sanctum.csrf-cookie":{"uri":"sanctum\/csrf-cookie","methods":["GET","HEAD"]},"home":{"uri":"\/","methods":["GET","HEAD"]},"storage.local":{"uri":"storage\/{path}","methods":["GET","HEAD"],"wheres":{"path":".*"},"parameters":["path"]}},"wildcards":{"sanctum.*":[],"storage.*":[]}}
-}
