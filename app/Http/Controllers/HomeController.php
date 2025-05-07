da04b61c3663784f5433d57c6e7eee15be54892a
nckrtl <nick.retel@gmail.com>
2025-05-07 10:47:12 +0200

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use NckRtl\RouteMaker\Route;
+
 class HomeController extends Controller
 {
+    #[Route(uri: '/')]
     public function show(): \Inertia\ResponseFactory|\Inertia\Response
     {
         return inertia('Home');
--- routes/route-maker.php
+++ routes/route-maker.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 // /
-Route::get('/home/{id}', [\App\Http\Controllers\HomeController::class, 'show'])->name('Controllers.HomeController.show');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'show'])->name('Controllers.HomeController.show');
 
